4330700d7bfbcc35e250fdeb2d56c2b5bf385a33
eliakip <kiplagateliakim52@gmail.com>
2025-06-02 22:40:59 +0300

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "POPKID;;;PB4RWC7I#drlnsjD568j3b4VxIJZ3gR4cjGyYfKVKkOFTh7jHQh4",
+SESSION_ID: process.env.SESSION_ID || "ELIAKIM;;;PB4RWC7I#drlnsjD568j3b4VxIJZ3gR4cjGyYfKVKkOFTh7jHQh4",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
@@ -13,7 +13,7 @@ AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
 // make true if you want auto reply on status 
 AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
 // make true if you want auto reply on status 
-AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*SEEN YOUR STATUS BY POPKID-MD 🤍*",
+AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*SEEN YOUR STATUS BY ELIAKIM-XMD 🤍*",
 // set the auto reply massage on status reply  
 WELCOME: process.env.WELCOME || "true",
 // true if want welcome and goodbye msg in groups    
@@ -27,9 +27,9 @@ MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files||.catbox.moe/e6rhto
 // add custom menu and mention reply image url
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot   
-BOT_NAME: process.env.BOT_NAME || "POPKID-MD",
+BOT_NAME: process.env.BOT_NAME || "ELIAKIM-XMD",
 // add bot namw here for menu
-STICKER_NAME: process.env.STICKER_NAME || "POPKID-MD",
+STICKER_NAME: process.env.STICKER_NAME || "ELIAKIM-XMD",
 // type sticker pack name 
 CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
 // make this true for custum emoji react    
@@ -37,15 +37,15 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "254732297194",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "254745931715",
 // add your bot owner number
-OWNER_NAME: process.env.OWNER_NAME || "Popkid Xtech",
+OWNER_NAME: process.env.OWNER_NAME || "Eliakim tech",
 // add bot owner name
-DESCRIPTION: process.env.DESCRIPTION || "*© powered by popkid*",
+DESCRIPTION: process.env.DESCRIPTION || "*© powered by eliakim*",
 // add bot owner name    
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/149k8x.jpg",
 // add img for alive msg
-LIVE_MSG: process.env.LIVE_MSG || "> AM ACTIVE *POPKID-MD*⚡",
+LIVE_MSG: process.env.LIVE_MSG || "> AM ACTIVE *ELIAKIM-XMD*⚡",
 // add alive msg here 
 READ_MESSAGE: process.env.READ_MESSAGE || "false",
 // Turn true or false for automatic read msgs
@@ -71,7 +71,7 @@ AUTO_TYPING: process.env.AUTO_TYPING || "false",
 // true for automatic show typing   
 READ_CMD: process.env.READ_CMD || "false",
 // true if want mark commands as read 
-DEV: process.env.DEV || "254732297195",
+DEV: process.env.DEV || "2547745931715",
 //replace with your whatsapp number        
 ANTI_VV: process.env.ANTI_VV || "true",
 // true for anti once view 
